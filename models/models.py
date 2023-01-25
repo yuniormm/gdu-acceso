5a0bd4daff35c0dae4f90fa2960019dc474ef233
Yunior Mustelier Mendez <yuniormm@uo.edu.cu>
2023-01-25 14:57:34 -0500

    Mejorando vista con js

--- models/models.py
+++ models/models.py
@@ -2,7 +2,8 @@
 
 from odoo import models, fields, api
 import logging
-import sys
+import sys, numbers, string
+import json
 
 limit = sys.getrecursionlimit()
 
@@ -28,21 +29,45 @@ class Escaneo(models.Model):
     _name = 'gdu.acceso.escaner'
     _description = 'GDU Acceso Scan'
     # _rec_name ='cod_barras'
-    cod_barras = fields.Char(string="Código de barras", size=11)
+    cod_barras = fields.Char(string="Código de barras", size=11, store=False)
+    nombre = fields.Char(string="Nombre:", readonly=True)
+    tipo_persona = fields.Char(string="Tipo de persona:", readonly=True)
+
 
     def f_search(self):
-        persona = self.env['gdu.base.persona'].search([('ci', '=', '83022125063')])
+        persona = self.env['gdu.base.persona'].search([('ci', '=', str(self.cod_barras))])
         #res.update({'warning': {'title': 'Warning !', 'message': 'Nombre: ' + str(persona.nombre)}})
-        print('search()', persona, persona.nombre)
-        return{'warning': {'title': 'Warning !', 'message': 'Nombre: ' + persona.nombre}}
+        #print('search()', persona, persona.nombre)
+        title = "Codigo de Barras"
+        message = "Persona encontrada: "+str(persona.nombre)
+        self.nombre = persona.nombre
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'display_notification',
+            'params': {
+                'title': title,
+                'message': message,
+                'sticky': False,
+            }
+        }
+
+        #return{'warning': {'title': 'Warning !', 'message': 'Nombre: ' + persona.nombre}}
 
     @api.onchange('cod_barras')
     def onchange_cod_barras(self):
         _logger.info('Entro al onchange')
         for record in self:
-            if record.cod_barras:
+            if str(record.cod_barras).isnumeric() and len(record.cod_barras)==11:
                 _logger.info('Entro al if, cod_barras %s' % record.cod_barras)
+                _logger.info('Count, cod_barras %s' % len(record.cod_barras))
+                persona = self.env['gdu.base.persona'].search([('ci', '=', str(record.cod_barras))])
+                record.nombre = persona.name
+                record.tipo_persona = persona.tipo_persona
                 self.env['gdu.acceso.logs'].create({'ci': record.cod_barras})
+                record.cod_barras = ""
+            else:
+                record.nombre = ""
+                record.tipo_persona = ""
 
     def limpia_cod_barras(self):
         self.cod_barras = "ahh bueno!"
