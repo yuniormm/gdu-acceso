8865507c3b82564b59edc5b8f891c3aca64614bf
Yunior Mustelier Mendez <yuniormm@uo.edu.cu>
2023-01-26 15:37:00 -0500

    Mejorando vista con html

--- models/models.py
+++ models/models.py
@@ -25,13 +25,15 @@ class Acceso(models.Model):
 
 
 # Prueba de Escaneo
-class Escaneo(models.Model):
+class Escaneo(models.TransientModel):
     _name = 'gdu.acceso.escaner'
     _description = 'GDU Acceso Scan'
     # _rec_name ='cod_barras'
     cod_barras = fields.Char(string="Código de barras", size=11, store=False)
-    nombre = fields.Char(string="Nombre:", readonly=True)
-    tipo_persona = fields.Char(string="Tipo de persona:", readonly=True)
+    nombre = fields.Char(string="", readonly=True)
+    tipo_persona = fields.Char(string="", readonly=True)
+    html = fields.Html(readonly=True)
+    #es_profesor = fields.Boolean('¿Es profesor?', readonly=True)
 
 
     def f_search(self):
@@ -55,19 +57,31 @@ class Escaneo(models.Model):
 
     @api.onchange('cod_barras')
     def onchange_cod_barras(self):
+        texthtml = ""
         _logger.info('Entro al onchange')
         for record in self:
             if str(record.cod_barras).isnumeric() and len(record.cod_barras)==11:
-                _logger.info('Entro al if, cod_barras %s' % record.cod_barras)
-                _logger.info('Count, cod_barras %s' % len(record.cod_barras))
+                #_logger.info('Entro al if, cod_barras %s' % record.cod_barras)
+                #_logger.info('Count, cod_barras %s' % len(record.cod_barras))
                 persona = self.env['gdu.base.persona'].search([('ci', '=', str(record.cod_barras))])
-                record.nombre = persona.name
-                record.tipo_persona = persona.tipo_persona
+
+                if(persona.name):
+                    texthtml += "<p class='text-success'><i class='fa fa-user fa-2x'></i>"+str(persona.name)+"</p>"
+                    texthtml += "<p class='text-success'>" + str(persona.tipo_persona) + "</p>"
+                    texthtml += "<p class='text-success'>" + str(persona.area_id) + "</p>"
+                    record.html = texthtml
+                    #record.nombre = persona.name
+                else:
+                    texthtml += "No identificado"
+                    record.html = texthtml
+
                 self.env['gdu.acceso.logs'].create({'ci': record.cod_barras})
                 record.cod_barras = ""
             else:
                 record.nombre = ""
                 record.tipo_persona = ""
+                record.cod_barras = ""
+
 
     def limpia_cod_barras(self):
         self.cod_barras = "ahh bueno!"
